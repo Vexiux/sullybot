234e46dd99e503b628bd3387373934ded3a44361
Vexiux <36649699+Vexiux@users.noreply.github.com>
2018-03-18 09:15:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Qbert?') {
+    	message.reply('I'm Qbert!');
   	}
 });
 
